1128c83fafb6cdb03e9f85987598765036eb1629
camneu37 <neuner.camille@gmail.com>
2018-06-26 12:26:20 -0700

    Add validations to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ApplicationRecord
   has_secure_password
+  validates :name, :email, :password, presence: true {message: "All fields must be completed."}
+  validates :email, uniqueness: true
 end

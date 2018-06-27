9da952908fb8d8a1c4bfa4b6b3b850538b959f23
camneu37 <neuner.camille@gmail.com>
2018-06-26 17:53:15 -0700

    Add test for valid email address; add validation for the same to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
   has_secure_password
   validates :name, :email, :password, presence: true
-  validates :email, uniqueness: true
+  validates :email, uniqueness: true, format: /@/
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -24,6 +24,11 @@ RSpec.describe User, type: :model do
     expect(test_user).to be_invalid
   end
 
+  it "must have a valid email address" do
+    test_user = User.create(name: "Tester", email: "tester", password: "tester")
+    expect(test_user).to be_invalid
+  end
+
 
 
 

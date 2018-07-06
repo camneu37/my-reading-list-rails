d8c7e3d3a627f6bfebab5d80ef4bd683cbe4b806
camneu37 <neuner.camille@gmail.com>
2018-07-05 21:33:28 -0700

    Update index view; Add show action to book controller; add show view for books

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,4 +4,14 @@ class BooksController < ApplicationController
     @books = Book.all
   end
 
+  def show
+    @book = book
+  end
+
+  private
+
+    def book
+      @book ||= Book.find(params[:id])
+    end
+
 end

93fcd4855152a5bb005ff5d4acee9fb8df0bec7a
camneu37 <neuner.camille@gmail.com>
2018-07-15 21:14:22 -0700

    update make_name_upcase method in author model

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,10 +1,10 @@
 class Author < ApplicationRecord
   validates :name, presence: true
   has_many :books
-  before_validation :make_name_upcase
+  before_save :make_name_upcase
 
   def make_name_upcase
-    self.name = self.name.split(" ").collect{|w| w.capitalize}.join(" ") if self.name
+    self.name = self.name.split(" ").collect{|w| w.capitalize}.join(" ")
   end
 
 end

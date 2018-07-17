07e2c71f206d94420cd8692d4d1f6f09c3d365ae
camneu37 <neuner.camille@gmail.com>
2018-07-16 21:01:28 -0700

    Add validation to author to prevent duplicates

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,10 +1,11 @@
 class Author < ApplicationRecord
   validates :name, presence: true
+  validates :name, uniqueness: true
   has_many :books
-  before_save :make_name_upcase
+  before_validation :make_name_upcase
 
   def make_name_upcase
-    self.name = self.name.split(" ").collect{|w| w.capitalize}.join(" ")
+    self.name = self.name.split(" ").collect{|w| w.capitalize}.join(" ") if self.name
   end
 
 end

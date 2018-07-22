d71807f2c90adb086d64b798a613a7c4cd94854b
camneu37 <neuner.camille@gmail.com>
2018-07-21 18:04:28 -0700

    Generate sessions resource

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   resources :books
   resources :users
   resources :authors, only: [:index, :show]
+  resources :sessions, only: [:new, :create, :destroy]
 
   root 'welcome#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html

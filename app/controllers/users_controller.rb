48db00364134df45e3c8f2d7ed8f195d78763ca4
camneu37 <neuner.camille@gmail.com>
2018-07-21 18:59:23 -0700

    Add tests for user account creation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,4 +3,8 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
+
+  def create
+    raise params.inspect
+  end
 end
--- spec/features/user_spec.rb
+++ spec/features/user_spec.rb
@@ -15,5 +15,43 @@ describe 'Feature Test: User Sign Up', type: :feature do
     expect(page).to have_content("Create an Account")
   end
 
+  it 'does not allow account creation without users name' do
+    visit '/users/new'
+    fill_in("user[username]", with: "test_user")
+    fill_in("user[password]", with: "test")
+    fill_in("user[password_confirmation]", with: "test")
+    click_button("Sign Up")
+    expect(page).to have_content("Create an Account")
+    expect(page).to have_content("User must have name")
+  end
+
+  it 'does not allow account creation without users username' do
+    visit '/users/new'
+    fill_in("user[name]", with: "Test User")
+    fill_in("user[password]", with: "test")
+    fill_in("user[password_confirmation]", with: "test")
+    click_button("Sign Up")
+    expect(page).to have_content("Create an Account")
+    expect(page).to have_content("User must have username")
+  end
+
+  it 'does not allow account creation without users password' do
+    visit '/users/new'
+    fill_in("user[name]", with: "Test User")
+    fill_in("user[username]", with: "test_user")
+    click_button("Sign Up")
+    expect(page).to have_content("Create an Account")
+    expect(page).to have_content("User must have password")
+  end
+
+  it 'does note allow account creation without password confirmation' do
+    visit '/users/new'
+    fill_in("user[name]", with: "Test User")
+    fill_in("user[username]", with: "test_user")
+    fill_in("user[password]", with: "test")
+    click_button("Sign Up")
+    expect(page).to have_content("Create an Account")
+    expect(page).to have_content("User must have password confirmation")
+  end
 
 end

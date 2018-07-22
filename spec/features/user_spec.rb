bf80e6fe424f929b0168df5608260693ef4b53c8
camneu37 <neuner.camille@gmail.com>
2018-07-21 19:37:40 -0700

    Add feature test section for users homepage

--- spec/features/user_spec.rb
+++ spec/features/user_spec.rb
@@ -73,7 +73,23 @@ describe 'Feature Test: User Sign Up', type: :feature do
     fill_in("user[password_confirmation]", with: "test")
     click_button("Sign Up")
     expect(current_path).to eq("/users/1")
-    expect(page).to have_content("Test User's Reading List")   
+    expect(page).to have_content("Test User's Reading List")
   end
+
+end
+
+describe 'Feature Test: Users Homepage', type: :feature do
+
+  before :each do
+    @author_one = Author.create(name: "Test Author 1")
+    @author_two = Author.create(name: "Test Author 2")
+    @book_one = @author_one.books.create(title: "Test Book 1", about: "A new test book", genre: "Fiction")
+    @book_two = @author_one.books.create(title: "Test Book 2", about: "A new test book", genre: "Non-fiction")
+    @book_three = @author_two.books.create(title: "Test Book 3", about: "A new test book", genre: "Fiction")
+    @user = User.create(name: "Test User", username: "test_user", password: "test", password_confirmation: "test")
+    @user.add_book_to_reading_list(@book_one)
+    @user.add_book_to_reading_list(@book_three)
+  end
+
 
 end

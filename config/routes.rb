beda6777add63a3150ed736aac7af6360a4b6035
camneu37 <neuner.camille@gmail.com>
2018-07-21 17:48:05 -0700

    Add controller, views, and route for welcome page

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   resources :books
   resources :users
   resources :authors, only: [:index, :show]
+
+  root 'welcome#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

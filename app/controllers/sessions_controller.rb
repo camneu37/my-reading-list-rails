62be81999cf947237ab8579ff78fd120a3d363e9
camneu37 <neuner.camille@gmail.com>
2018-07-23 21:41:22 -0700

    Add uid column to users table; Update session create to include FB login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,9 +7,18 @@ class SessionsController < ApplicationController
   end
 
   def create
-    binding.pry
     @user = User.find_by(username: params[:username])
-    if params[:username].empty? || params[:password].empty?
+    if auth
+      @user = User.find_or_create_by(uid: auth[:uid]) do |u|
+        u.name = auth[:info][:name]
+        u.username = auth[:info][:name]
+        u.password = SecureRandom.hex
+        u.password_confirmation = u.password
+      end
+      binding.pry
+      session[:user_id] = @user.id
+      redirect_to user_path(@user)
+    elsif params[:username].empty? || params[:password].empty?
       flash[:message] = "Must fill in username and password fields"
       render :new
     elsif @user.nil?
@@ -29,5 +38,10 @@ class SessionsController < ApplicationController
     @current_user = nil
     redirect_to root_path
   end
+
+  private
+    def auth
+      request.env['omniauth.auth']
+    end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_22_172849) do
+ActiveRecord::Schema.define(version: 2018_07_24_044038) do
 
   create_table "authors", force: :cascade do |t|
     t.string "name"
@@ -50,6 +50,7 @@ ActiveRecord::Schema.define(version: 2018_07_22_172849) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "uid"
   end
 
 end

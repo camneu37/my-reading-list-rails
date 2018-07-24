776dc247df932d5ae3d57797885aec41c33fbb3a
camneu37 <neuner.camille@gmail.com>
2018-07-23 21:00:33 -0700

    remove google sign up; add links and routes for signing up/logging in with facebook

--- config/routes.rb
+++ config/routes.rb
@@ -10,5 +10,7 @@ Rails.application.routes.draw do
 
   post '/books/:id/add', to: 'books#add', as: 'add_book'
   post '/books/:id/remove', to: 'books#remove', as: 'remove_book'
+
+  get '/auth/facebook/callback', to: 'sessions#create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

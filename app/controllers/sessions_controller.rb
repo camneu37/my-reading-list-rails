033e4655bcb632dcd839d1734fdc593d1bd24b04
camneu37 <neuner.camille@gmail.com>
2018-07-23 21:21:00 -0700

    Update files with Facebook omniauth details

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,6 +7,7 @@ class SessionsController < ApplicationController
   end
 
   def create
+    binding.pry
     @user = User.find_by(username: params[:username])
     if params[:username].empty? || params[:password].empty?
       flash[:message] = "Must fill in username and password fields"

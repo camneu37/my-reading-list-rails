1d85370d3331dabf2f1a742f604be400c78c269d
camneu37 <neuner.camille@gmail.com>
2018-08-06 22:34:43 -0700

    Update create method to get it working

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -1,21 +1,23 @@
 class RatingsController < ApplicationController
 
+  #NEED REFACTOR THIS!
   def create
-    binding.pry
     if params[:book_id]
       book = Book.find(params[:book_id])
-      if book.already_rated_by_user(current_user)
+      if book.already_rated_by_user?(current_user)
         flash[:message] = "You've already submitted a rating for this book."
       else
         rating = book.ratings.create(rating: params[:rating], user_id: current_user.id)
       end
+      redirect_to book_path(book)
     elsif params[:author_id]
       author = Author.find(params[:author_id])
-      if current_user.has_already_rated?(author)
+      if author.already_rated_by_user?(current_user)
         flash[:message] = "You've already submitted a rating for this book."
       else
         rating = author.ratings.create(rating: params[:rating], user_id: current_user.id)
       end
+      rediect_to author_path(author)
     end
   end
 
